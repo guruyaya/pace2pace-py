b3493621c9b639a5e1c6d62574794a72f51e59a6
Yair Eshel Cahansky <guruyaya@gmail.com>
2020-11-13 18:11:54 +0200

    pace2pace redesign - request

--- pace2pace/pace2pace.py
+++ pace2pace/pace2pace.py
@@ -2,166 +2,37 @@
 import requests
 import json
 import pgpy
-import sys
-import re
+import time
 
-from pace2pace_exceptions import Pace2PaceCouldNotLoadDocException, Pace2PaceDocSigInvalid, Pace2PaceUrlNotListedInDoc
+VERSION = 0.1
 
-class Pace2PaceActions:
-    @staticmethod
-    def eprint(args):
-        sys.stderr.write(args)
-
-    @staticmethod
-    def testdoc(*params):
-        help_text = '''Usage: pace2pace.py testdoc --url=<doc url> [--url=<additional url> ...]
-'''
-        urls = []
-        next = None
-        for param in params:
-            if next == None:
-                if param in ('-u', '--url'):
-                    next = 'url'
-                else:
-                    Pace2PaceActions.eprint('Unknown param %s\n' % param)
-                    Pace2PaceActions.eprint(help_text)
-                    sys.exit(-1)
-            elif next == 'url':
-                urls += [param]
-            else:
-                Pace2PaceActions.eprint('Unknown param %s\n' % param)
-                Pace2PaceActions.eprint(help_text)
-                sys.exit(-1)
-
-        if len(urls) == 0:
-            Pace2PaceActions.eprint('Error, at least one url must be provided\n')
-            Pace2PaceActions.eprint(help_text)
-            sys.exit(-1)
-
-        docReader = DocReader(urls)
-        docReader.load()
-        docReader.validate()
-        print docReader
-
-class DocReader:
-    docText = None
-    jsonText = None
-    signature = None
-    messageObject = None
-    isVerified = False
-    checkedUrl = None
-
-    def __init__(self, urls, assumeValid=False):
-        self.urls = urls
-        self.assumeValid = assumeValid
-
-    def parseDoc(self):
-        self.pgpMessage = pgpy.PGPMessage()
-        self.pgpMessage.parse(re.compile('\r\n').sub('\n',self.urlRes.text.encode('UTF8')))
-
-    def parseJsonMessage(self):
-        self.messageObject = json.loads(self.pgpMessage.message)
-
-    def getAllowedUrls(self):
-        return self.messageObject['urls']
-
-    def getKey(self, key):
-        pgp_key = pgpy.PGPKey()
-        pgp_key.parse(self.messageObject['keys'][key]['key'])
-        return pgp_key
-
-    def load(self):
-        self.urlRes = self.getDocFromUrls();
-        self.parseDoc()
-        self.parseJsonMessage()
-
-    def validate(self):
-        # check if url in urls list
-        if self.checkedUrl not in self.getAllowedUrls():
-            raise Pace2PaceUrlNotListedInDoc(self.checkedUrl)
-
-        rootKey = self.getKey('_ROOT')
-        verifier = rootKey.verify(self.pgpMessage)
-        if (not verifier):
-            raise Pace2PaceDocSigInvalid(self.checkedUrl)
-        self.isVerified = True
-
-    def getDocFromUrls(self):
-        for url in self.urls:
-            try:
-                self.checkedUrl = url
-                return requests.get(url)
-            except:
-                pass
-        else:
-            raise Pace2PaceCouldNotLoadDocException(self.urls)
-
-    def __str__(self):
-        return 'Pace2PaceDoc(urls={}, valid={})'.format(self.urls, self.isVerified)
-    def __bool__(self):
-        return self.isVerified
-
-class RequestProceesor:
-    ''' This class gets a request and handles it'''
-    request = None
-    isValid = False
-    doc = None
-    validationKeyName = None
-    validationKey = None
-
-    def __init__(self, requestJson):
-        self.requestJson = requestJson
-
-    def load(self):
-        # parse the reuqest json
-        self.request = json.loads(requestJson)
-        # load the attached doc
-        self.doc = DocReader(self.request['urls'])
-        self.doc.load()
-        self.doc.validate()
-        # get the validation key
-
-
-    def validate(self):
-        # laod key
-        # validate against requested key
-        pass
-
-class BaseLoginValidation:
-    ''' This class handles the process of validating login process
-note that this is the basic implemetation. The specific implementation of the protocol, must be applied as extention of this class
+class Pace2PaceMasterRequest():
+    ''' handles the creation of Pace2Pace request from master key provider
     '''
-    def __init__(self, privateKey, encodedRequest):
-        self.privateKey = privateKey
-        self.encodedRequest = encodedRequest
-        self.validationString = validationString
-
-    def load(self):
-        # decode the endcoded request
-        # validate string
-        # load / validate doc
-        # validate request
-        pass
-
-    def getValidationString(self):
+    def __init__(self, action: str, data: dict, timestamp: int = None, version: float =VERSION):
+        ''' Builds a Pace2Pace request object
+        :param action: gets any of the actions
+        :param dict: gets a dict with action params
+        :return: returns nothing
         '''
+        self.action = action
+        self.data = data
+        if version > VERSION:
+            raise NotImplementedError('Your installed version support only version %.2f and down' % VERSION) 
+        self.version = version
+        self.timestamp = int( time.time() ) if timestamp == None else timestamp
+        print (self.timestamp)
+
+    def to_json(self):
+        ''' Returns a json representation of the unsigned request'''
+        return json.dumps({'Pace2Pace': {
+            'version': self.version, 'action': self.action, 
+            'data': self.data, 'timestamp': self.timestamp
+        }})
+
+    def send_to_master_via_file(self, file=None):
+        '''Sends request to master
+        :param file: is either filename or handler, required for file 
         '''
-        pass
-
-    def validate(self):
-        pass
-
-if __name__ == '__main__':
-    action = None
-    if len(sys.argv) > 1:
-        action = sys.argv[1]
-
-    if action in ('testdoc',):
-        Pace2PaceActions.testdoc(*sys.argv[2:])
-    else:
-        Pace2PaceActions.eprint('''
-Usage: pace2pace.py <action> [action params]
-
-Actions: testdoc - checks url for doc validity
-''')
-        sys.exit(-1)
+        with open(file, 'w') as f:
+            f.write(self.to_json())
---
+++ pace2pace/pace2pace_test.py
@@ -0,0 +1,41 @@
+import unittest
+import tempfile
+import os
+import json
+from pace2pace import Pace2PaceMasterRequest
+
+class pace2pace_test(unittest.TestCase):
+    def test_gen_request(self):
+        genreq = Pace2PaceMasterRequest('this', {'is': 'a test'})
+        self.assertEqual(genreq.action, 'this')
+        self.assertEqual(genreq.data, {'is': 'a test'})
+        self.assertEqual(genreq.version, 0.1)
+
+        # errors
+        with self.assertRaises(NotImplementedError) as context:
+            Pace2PaceMasterRequest('this', {'is': 'a test'}, version=0.2)
+
+        self.assertEqual('Your installed version support only version 0.10 and down', 
+            context.exception.args[0])
+
+    def test_to_json(self):
+        genreq = Pace2PaceMasterRequest('this', {'is': 'a test'})
+        self.assertRegex(genreq.to_json(), 
+            '{"Pace2Pace": {"version": 0.1, "action": "this", "data": {"is": "a test"}, "timestamp": [0-9]+}}')
+    
+    def test_send_to_master_via_file(self):
+        genreq = Pace2PaceMasterRequest('this', {'is': 'a test'})
+        handle, temp_sig = tempfile.mkstemp()
+        try:
+            genreq.send_to_master_via_file(handle)
+            with open(temp_sig, 'r') as f:
+                newreq = json.load(f)
+                self.assertEqual(newreq['Pace2Pace']['action'], 'this')
+                self.assertEqual(newreq['Pace2Pace']['data'], {'is': 'a test'})
+                self.assertIsInstance(newreq['Pace2Pace']['timestamp'], int)
+                self.assertEqual(newreq['Pace2Pace']['version'], 0.1)
+        finally:
+            os.unlink (temp_sig)
+
+if __name__ == '__main__':
+    unittest.main(argv=[''],verbosity=2, exit=False)
